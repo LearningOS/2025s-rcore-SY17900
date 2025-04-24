d0407bdb9d9f988018397f4a3a22e48b52addd40
SY17900 <2332681550@qq.com>
2025-04-24 23:14:24 +0800

    sys_get_time ok

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,5 +1,7 @@
 //! Process management syscalls
-use crate::task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next};
+use core::slice::from_raw_parts;
+
+use crate::{mm::translated_byte_buffer, task::{change_program_brk, current_user_token, exit_current_and_run_next, suspend_current_and_run_next}, timer::get_time_us};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -25,9 +27,30 @@ pub fn sys_yield() -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     trace!("kernel: sys_get_time");
-    -1
+    let us = get_time_us();
+    let ref tv = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+    let source = tv as *const TimeVal;
+    let mut start_ptr = source as *const u8;
+    let targets = translated_byte_buffer(
+        current_user_token(),
+    ts as *const u8,
+        core::mem::size_of::<TimeVal>()
+    );
+
+    for target in targets {
+        let length = target.len();
+        unsafe {
+            target.copy_from_slice(from_raw_parts(start_ptr, length));
+            start_ptr = start_ptr.add(length);
+        }
+    }
+
+    0
 }
 
 /// TODO: Finish sys_trace to pass testcases
